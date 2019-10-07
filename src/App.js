dec8465004bd5bc43dc848754c1b38ca78950690
dumpstertier <joni.miromaki@gmail.com>
2019-10-08 00:51:44 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,182 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container, Grid, FormControl, InputLabel, Select, Button } from '@material-ui/core';
+import Question from './Question';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.fetchData = this.fetchData.bind(this);
+    this.restart = this.restart.bind(this);
+    this.addScore = this.addScore.bind(this);
+    this.nextQuestion = this.nextQuestion.bind(this);
+    this.state = {
+      category: '',
+      difficulty: '',
+      score: 0,
+      currentQuestion: 0,
+      questions: null,
+      isError: false
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  fetchData(event) {
+    event.preventDefault();
+    let apiUrl = "https://opentdb.com/api.php?amount=10&category=" + this.state.category + "&difficulty=" + this.state.difficulty;
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            questions: result.results
+          });
+        },
+        (error) => {
+          console.log(error);
+          this.setState({
+            isError: true
+          });
+        }
+      )
+  }
+
+  addScore() {
+    let score = this.state.score;
+    score++;
+    this.setState({
+      score: score
+    });
+  }
+
+  nextQuestion() {
+    let currentQuestion = this.state.currentQuestion;
+    currentQuestion++;
+    console.log(currentQuestion);
+    this.setState({
+      currentQuestion: currentQuestion
+    });
+  }
+
+  restart(event) {
+    event.preventDefault();
+    this.setState({
+      category: '',
+      difficulty: '',
+      score: 0,
+      questions: null,
+      currentQuestion: 0
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>OTrivia</h1>
+        </header>
+        <main>
+          {this.state.questions ? (
+            <Container maxWidth="sm">
+              <Grid container spacing={4}>
+                <Grid item xs={12}>
+                  <p>{this.state.currentQuestion + 1}/10</p>
+                  <p>Current score: {this.state.score}</p>
+                </Grid>
+                {this.state.questions.map((question, index) =>
+                  <Grid
+                    item xs={12}
+                    key={index}
+                    className={this.state.currentQuestion === index ? "Question current" : "Question"}
+                  >
+                    <Question question={question} score={this.state.score} addScore={this.addScore} nextQuestion={this.nextQuestion} restart={this.restart} index={index} />
+                  </Grid>
+                )}
+              </Grid>
+            </Container>
+          ) : this.state.isError === true ? (
+            <Container maxWidth="sm">
+              Error connecting to database.
+            </Container>
+          ) : (
+            <Container maxWidth="sm">
+              <form>
+                <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <FormControl>
+                      <InputLabel htmlFor="category">Category</InputLabel>
+                      <Select
+                        value={this.state.category}
+                        defaultValue='any'
+                        onChange={this.handleChange}
+                        inputProps={{
+                          name: 'category',
+                          id: 'category'
+                        }}
+                      >
+                        <option value={"any"}>Any Category</option>
+                        <option value={"9"}>General Knowledge</option>
+                        <option value={"10"}>Entertainment: Books</option>
+                        <option value={"11"}>Entertainment: Film</option>
+                        <option value={"12"}>Entertainment: Music</option>
+                        <option value={"13"}>Entertainment: Musicals &amp; Theatres</option>
+                        <option value={"14"}>Entertainment: Television</option>
+                        <option value={"15"}>Entertainment: Video Games</option>
+                        <option value={"16"}>Entertainment: Board Games</option>
+                        <option value={"17"}>Science &amp; Nature</option>
+                        <option value={"18"}>Science: Computers</option>
+                        <option value={"19"}>Science: Mathematics</option>
+                        <option value={"20"}>Mythology</option>
+                        <option value={"21"}>Sports</option>
+                        <option value={"22"}>Geography</option>
+                        <option value={"23"}>History</option>
+                        <option value={"24"}>Politics</option>
+                        <option value={"25"}>Art</option>
+                        <option value={"26"}>Celebrities</option>
+                        <option value={"27"}>Animals</option>
+                        <option value={"28"}>Vehicles</option>
+                        <option value={"29"}>Entertainment: Comics</option>
+                        <option value={"30"}>Science: Gadgets</option>
+                        <option value={"31"}>Entertainment: Japanese Anime &amp; Manga</option>
+                        <option value={"32"}>Entertainment: Cartoon &amp; Animations</option>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <FormControl>
+                      <InputLabel htmlFor="difficulty">Difficulty</InputLabel>
+                      <Select
+                        value={this.state.difficulty}
+                        defaultValue='any'
+                        onChange={this.handleChange}
+                        inputProps={{
+                          name: 'difficulty',
+                          id: 'difficulty'
+                        }}
+                      >
+                        <option value={"any"}>Any Difficulty</option>
+                        <option value={"easy"}>Easy</option>
+                        <option value={"medium"}>Medium</option>
+                        <option value={"hard"}>Hard</option>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button variant="outlined" size="large" color="primary" onClick={this.fetchData}>Start</Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </Container>
+          )}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Question.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Grid, Button } from '@material-ui/core';
+import './Question.css';
+
+class Question extends React.Component {
+  constructor(props) {
+    super(props);
+    this.checkAnswer = this.checkAnswer.bind(this);
+    this.state = {
+      answers: [],
+      isCorrect: false,
+      answered: false
+    }
+  }
+
+  checkAnswer(answer) {
+    if (answer === this.props.question.correct_answer) {
+      this.setState({
+        isCorrect: true
+      });
+      this.props.addScore();
+    }
+
+    this.setState({
+      answered: true
+    });
+  }
+
+  componentDidMount() {
+    let answers = this.props.question.incorrect_answers;
+    answers.push(this.props.question.correct_answer);
+
+    for (let i = answers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const tmp = answers[i];
+      answers[i] = answers[j];
+      answers[j] = tmp;
+    }
+
+    this.setState({
+      answers: answers
+    });
+  }
+
+  render() {
+    return(
+      <Grid container spacing={3}>
+        <Grid item xs={12}>{this.props.question.question}</Grid>
+        {this.state.answers.map((answer, index) =>
+          <Grid item sm={6} xs={12}>
+            <Button
+              variant="outlined"
+              size="medium"
+              color="secondary"
+              onClick={() => this.checkAnswer(answer)}
+              disabled={this.state.answered ? true : false}
+              key={index}
+              className={this.state.answered && answer === this.props.question.correct_answer ? "AnswerButton green" : this.state.answered && answer !== this.props.question.correct_answer ? "AnswerButton red" : "AnswerButton"}
+            >
+              {answer}
+            </Button>
+          </Grid>
+        )}
+        {this.props.index === 9 && this.state.answered ? (
+          <Grid item xs={12}>
+            <p>Total score: {this.props.score}</p>
+            <Button variant="outlined" size="large" color="primary" onClick={this.props.restart}>Start again</Button>
+          </Grid>
+        ) : this.props.index !== 9 && this.state.answered ? (
+          <Grid item xs={12}>
+            <Button variant="outlined" size="large" color="primary" onClick={this.props.nextQuestion}>Next question</Button>
+          </Grid>
+        ) : ""}
+      </Grid>
+    );
+  }
+}
+
+export default Question;
