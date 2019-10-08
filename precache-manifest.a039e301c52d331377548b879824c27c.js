3434a8747f58dd3370f42f231ed0713203a985a9
dumpstertier <joni.miromaki@gmail.com>
2019-10-08 14:46:11 +0300

    Updates

---
+++ precache-manifest.a039e301c52d331377548b879824c27c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2119a6245babb613909717fd3252c5a",
+    "url": "/otrivia/index.html"
+  },
+  {
+    "revision": "e8c91191377f529300b8",
+    "url": "/otrivia/static/css/main.f2de61f2.chunk.css"
+  },
+  {
+    "revision": "08f72c510e342071a31b",
+    "url": "/otrivia/static/js/2.2d8e9cd3.chunk.js"
+  },
+  {
+    "revision": "e8c91191377f529300b8",
+    "url": "/otrivia/static/js/main.b5b75b70.chunk.js"
+  },
+  {
+    "revision": "b2313094bbe9470b45e8",
+    "url": "/otrivia/static/js/runtime-main.49d374e2.js"
+  }
+]);
